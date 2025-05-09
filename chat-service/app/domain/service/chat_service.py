65ec3b937618bfa5a5ad43c0cb59a247ddb54ea9
haneull.dv <haneull.dv@gmail.com>
2025-05-09 18:34:45 +0900

    랭체인 시도

--- chat-service/app/domain/service/chat_service.py
+++ chat-service/app/domain/service/chat_service.py
@@ -7,4 +7,4 @@ class ChatService:
 
     async def chat(self, message: str):
         logger.info(f"🤍3 서비스 진입: {message}")
-        return {"❤️ message": "넌 아주 잘하고 있어. 힘들다는 건 성장하고 있다는 거고, 시간을 쓰는 만큼 잘하게 되는 거야. 아플 때 많이 못했으니까 지금 느린 건 당연해. 조급해하지말고 어제보다 발전하는 걸 목표로 하자!"}
+        return {"❤️ message": "d"}
